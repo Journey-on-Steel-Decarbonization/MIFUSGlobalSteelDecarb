558d13401681ab5ac7ffd39cf9a392136c535dc0
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-11-16 18:07:12 -0300

    Script to move PDF files to a separate folder

---
+++ .vscode/move_pdf_files.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -o xtrace
+PS4='${LINENO}: '
+
+pushd .
+mkdir PDFs
+mv "BA_SteelDecarbonizationattheCrossroads.pdf" "PDFs/BA_SteelDecarbonizationattheCrossroads.pdf"
+mv "BC_SteelGDP.pdf" "PDFs/BC_SteelGDP.pdf"
+mv "BD_SteelProCapita.pdf" "PDFs/BD_SteelProCapita.pdf"
+mv "C_ChinaSteelPolicyDeep01.pdf" "PDFs/C_ChinaSteelPolicyDeep01.pdf"
+mv "D_ClaudeIndia.pdf" "PDFs/D_ClaudeIndia.pdf"
+mv "FabioXAndre_AntonioAugusto_Dickson.pdf" "PDFs/FabioXAndre_AntonioAugusto_Dickson.pdf"
+mv "H_Germany.pdf" "PDFs/H_Germany.pdf"
+mv "H_SouthKoreaClaude.pdf" "PDFs/H_SouthKoreaClaude.pdf"
+mv "J_EuropeanUnion.pdf" "PDFs/J_EuropeanUnion.pdf"
+mv "K_South_Korea.pdf" "PDFs/K_South_Korea.pdf"
+mv "L_TurkeyPerplexity.pdf" "PDFs/L_TurkeyPerplexity.pdf"
+mv "M_Brasil.pdf" "PDFs/M_Brasil.pdf"
+mv "M_Brasil_ClaudeAndreFabio.pdf" "PDFs/M_Brasil_ClaudeAndreFabio.pdf"
+mv "O_Vietnam.pdf" "PDFs/O_Vietnam.pdf"
+mv "P_Egypt.pdf" "PDFs/P_Egypt.pdf"
+mv "Q_Oman.pdf" "PDFs/Q_Oman.pdf"
+mv "R_Indonesia.pdf" "PDFs/R_Indonesia.pdf"
+mv "R_Indonesia_Claude.pdf" "PDFs/R_Indonesia_Claude.pdf"
+mv "R_Indonesia_Grok.pdf" "PDFs/R_Indonesia_Grok.pdf"
+mv "S_Taiwan.pdf" "PDFs/S_Taiwan.pdf"
+mv "SteelDecarbonizationattheCrossroads.pdf" "PDFs/SteelDecarbonizationattheCrossroads.pdf"
+mv "SteelGDP.pdf" "PDFs/SteelGDP.pdf"
+mv "SteelProCapita.pdf" "PDFs/SteelProCapita.pdf"
+mv "T_Mexico.pdf" "PDFs/T_Mexico.pdf"
+mv "T_MexicoClaudeDeepfabio.pdf" "PDFs/T_MexicoClaudeDeepfabio.pdf"
+mv "T_MexicoRemix.pdf" "PDFs/T_MexicoRemix.pdf"
+mv "U_Canada.pdf" "PDFs/U_Canada.pdf"
+mv "V_Spain_ESP.pdf" "PDFs/V_Spain_ESP.pdf"
+mv "W_France.pdf" "PDFs/W_France.pdf"
+mv "W_France_Fr.pdf" "PDFs/W_France_Fr.pdf"
+mv "X_Egypt.pdf" "PDFs/X_Egypt.pdf"
+
+popd
