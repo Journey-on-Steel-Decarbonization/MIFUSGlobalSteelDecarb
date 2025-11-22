2e4941c5978e38b2cbc1dd25a1815c2f6c4d72b6
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-11-21 22:21:19 -0300

    Draft script to remove auxiliary files generated by TeX tools

---
+++ scripts/remove_auxiliary_tex_files.py
@@ -0,0 +1,101 @@
+"""
+Script to remove TeX auxiliary files
+"""
+
+import inspect
+import os.path
+from pathlib import Path
+
+
+def remove_auxiliary_tex_files():
+    file_extensions = get_auxiliary_tex_file_extensions()
+    root_folder = get_repo_root_folder()
+
+    for directory, dirnames, filenames in os.walk(root_folder):
+        excluded_dirs = [".git", ".venv", ".ruff_cache"]
+        excluded_dirs_flag = [(e in directory) for e in excluded_dirs]
+        if any(excluded_dirs_flag):
+            continue
+        print(directory)
+
+        for filename in filenames:
+            extension_to_exclude = [filename.endswith(e) for e in file_extensions]
+            full_file_name = os.path.join(directory, filename)
+            if any(extension_to_exclude):
+                os.remove(full_file_name)
+
+
+def get_auxiliary_tex_file_extensions() -> list[str]:
+    """
+    Get auxiliary TeX file extensions
+    """
+    file_extensions = ["log"]
+
+    root_folder = get_repo_root_folder()
+    os.chdir(root_folder)
+
+    if os.path.exists(".gitignore"):
+        with open(".gitignore", mode="r", encoding="utf-8") as file:
+            line = file.readline()
+            header_found = False
+
+            while line:
+                if header_found and len(line) >= 4:
+                    stripped_line = line.strip()
+                    extension = stripped_line.split(".")[1:]
+                    extension = ".".join(extension)
+                    file_extensions.append(extension)
+
+                if line.strip() == "# TeX support files":
+                    header_found = True
+                elif line.startswith("#") and header_found:
+                    break
+
+                line = file.readline()
+    else:
+        raise FileNotFoundError(".gitignore file not found in root folder")
+
+    return file_extensions
+
+
+def get_repo_root_folder():
+    """
+    Get root folder of repository
+    """
+    current_folder = Path(get_current_folder())
+    parent = current_folder
+    dirnames = os.listdir(current_folder)
+
+    max_levels = 10
+    counter = 0
+
+    while ".git" not in dirnames:
+        parent = parent.parent
+        dirnames = os.listdir(parent)
+        counter += 1
+        if counter > max_levels:
+            break
+
+    if ".git" in dirnames:
+        root_folder = parent
+    else:
+        root_folder = ""
+
+    return root_folder
+
+
+def get_current_folder() -> str:
+    filename = inspect.getframeinfo(inspect.currentframe()).filename
+    path = os.path.dirname(os.path.abspath(filename))
+    return path
+
+
+def main():
+    current_folder = get_current_folder()
+    extensions_to_delete = get_auxiliary_tex_file_extensions()
+    print(extensions_to_delete)
+    remove_auxiliary_tex_files()
+
+
+if __name__ == "__main__":
+    main()
